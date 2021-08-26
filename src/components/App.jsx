fdb13a1e1565d0e746f0c3f18289bf21b69183dd
William <will2b@gmail.com>
2021-08-26 09:51:20 -0700

    housekeeping

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import logo from "../images/logo.svg";
+import "../style/App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App bg-red-200">App</div>;
+}
+
+export default App;
