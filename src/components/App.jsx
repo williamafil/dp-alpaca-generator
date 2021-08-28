1ea49c2fafd513afcf891867c23b8c0053adafcf
William <will2b@gmail.com>
2021-08-28 12:55:13 -0700

    add download img function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -65,6 +65,24 @@ class App extends React.Component {
       neck: "default",
       nose: "default",
     };
+    this.canvasRef = React.createRef();
+    this.imageRef = React.createRef();
+    this.accessoriesRef = React.createRef();
+    this.backgroundRef = React.createRef();
+    this.earsRef = React.createRef();
+    this.eyesRef = React.createRef();
+    this.hairRef = React.createRef();
+    this.legRef = React.createRef();
+    this.mouthRef = React.createRef();
+    this.neckRef = React.createRef();
+    this.noseRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const bg = this.backgroundRef.current;
+    bg.onload = () => {
+      console.log("bg onloaded");
+    };
   }
 
   onChangeSectionHandler = (e) => {
@@ -93,6 +111,50 @@ class App extends React.Component {
     this.setState({ ...this.state, ...obj });
   };
 
+  toImage = () => {
+    // const canvas = this.refs.canvas;
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const bgEl = this.backgroundRef.current;
+    const hairEl = this.hairRef.current;
+    const earsEl = this.earsRef.current;
+    const neckEl = this.neckRef.current;
+    const noseEl = this.noseRef.current;
+    const mouthEl = this.mouthRef.current;
+    const accessoriesEl = this.accessoriesRef.current;
+    const eyesEl = this.eyesRef.current;
+    const legEl = this.legRef.current;
+    const img = this.imageRef.current;
+
+    console.log(canvas);
+    console.log(ctx);
+    const imgs = img.querySelectorAll("img");
+
+    ctx.drawImage(bgEl, 0, 0, bgEl.width, bgEl.height);
+    ctx.drawImage(neckEl, 0, 0, neckEl.width, neckEl.height);
+    ctx.drawImage(earsEl, 0, 0, earsEl.width, earsEl.height);
+    ctx.drawImage(noseEl, 0, 0, noseEl.width, noseEl.height);
+    ctx.drawImage(mouthEl, 0, 0, mouthEl.width, mouthEl.height);
+    ctx.drawImage(hairEl, 0, 0, hairEl.width, hairEl.height);
+    ctx.drawImage(
+      accessoriesEl,
+      0,
+      0,
+      accessoriesEl.width,
+      accessoriesEl.height,
+    );
+    ctx.drawImage(eyesEl, 0, 0, eyesEl.width, eyesEl.height);
+    ctx.drawImage(legEl, 0, 0, legEl.width, legEl.height);
+
+    const link = document.createElement("a");
+
+    link.href = canvas.toDataURL("image/png");
+    link.download = "alpaca.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   render() {
     const imgUrl = new URL("/images/", import.meta.url).href;
 
@@ -105,43 +167,52 @@ class App extends React.Component {
         </header>
         <main className="flex flex-col sm:flex-row mx-auto">
           <section className=" sm:w-1/2">
-            <div className="relative leading-none">
+            <div ref={this.imageRef} className="relative leading-none">
               <img
+                ref={this.neckRef}
                 className="absolute"
                 src={imgUrl + "/neck/" + this.state.neck + ".png"}
               />
               <img
+                ref={this.earsRef}
                 className="absolute"
                 src={imgUrl + "/ears/" + this.state.ears + ".png"}
               />
 
               <img
+                ref={this.noseRef}
                 className="absolute"
                 src={imgUrl + "/nose/" + this.state.nose + ".png"}
               />
               <img
+                ref={this.mouthRef}
                 className="absolute"
                 src={imgUrl + "/mouth/" + this.state.mouth + ".png"}
               />
 
               <img
+                ref={this.hairRef}
                 className="absolute"
                 src={imgUrl + "/hair/" + this.state.hair + ".png"}
               />
               <img
+                ref={this.accessoriesRef}
                 className="absolute"
                 src={imgUrl + "/accessories/" + this.state.accessories + ".png"}
               />
               <img
+                ref={this.eyesRef}
                 className="absolute"
                 src={imgUrl + "/eyes/" + this.state.eyes + ".png"}
               />
 
               <img
+                ref={this.legRef}
                 className="absolute"
                 src={imgUrl + "/leg/" + this.state.leg + ".png"}
               />
               <img
+                ref={this.backgroundRef}
                 className=""
                 src={imgUrl + "/backgrounds/" + this.state.backgrounds + ".png"}
               />
@@ -159,7 +230,10 @@ class App extends React.Component {
               >
                 Random
               </button>
-              <button className="py-3 px-8 bg-white rounded-md text-md  text-black font-extrabold border-2 hover:border-gray-600 hover:shadow-md">
+              <button
+                className="py-3 px-8 bg-white rounded-md text-md  text-black font-extrabold border-2 hover:border-gray-600 hover:shadow-md"
+                onClick={this.toImage}
+              >
                 Download
               </button>
             </div>
@@ -201,6 +275,12 @@ class App extends React.Component {
             </RadioGroup>
           </section>
         </main>
+        <canvas
+          className="hidden"
+          ref={this.canvasRef}
+          width={535}
+          height={535}
+        />
       </div>
     );
   }
