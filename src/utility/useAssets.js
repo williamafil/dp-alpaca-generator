401d9cc7c41e2e4d432bcd17c36d76c6c5d3190d
William <will2b@gmail.com>
2021-08-28 10:25:48 -0700

    remove useAsset

--- src/utility/useAssets.js
+++
@@ -1,6 +0,0 @@
-// export default function useAssets() {
-//   const pngs = import.meta.glob("/src/images/backgrounds/\*.png");
-
-//   return {
-//     backgrounds: pngs["/src/images/backgrounds/*.png"]
-//   }
